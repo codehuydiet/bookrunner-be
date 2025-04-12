2cf188fa8ffd665df28ef4b6952bd599ef30e37c
codehuydiet <hoangnv1415@gmail.com>
2025-04-12 12:12:14 +0700

    first commit

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['https://bookrunner-fe-cx3s.vercel.app', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
